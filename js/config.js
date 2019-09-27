5eea704d07b90b9ff0bed86990d5e0fe500c431b
Ma Chi <hellomarch@gmail.com>
2019-09-27 13:09:15 +0000

    Change client ID

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_gRFpRu9gH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4laa0el2iibselknr0oifqdlin', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2oks6dbgs53oumovi0nf407995', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
