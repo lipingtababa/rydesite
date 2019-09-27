042be057a26b721042630dddc20bcc6c06fc04b3
Ma Chi <hellomarch@gmail.com>
2019-09-27 14:19:13 +0000

    zip compress the lambda function

--- infr/lambda.tf
+++ infr/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "web_lambda" {
-	filename      = "./requestUnicorn.js"
+	filename      = "./requestUnicorn.js.zip"
 	function_name = "RequestUnicorn"
 	role          = "${aws_iam_role.web_role.arn}"
 	handler       = "exports.test"
